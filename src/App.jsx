688976b75e3e9d3be6f27a575e0b303c122983f3
elisabichiappini <elisabichiappini@gmail.com>
2026-05-20 15:14:43 +0200

    feat: add main

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import Header from './components/Header/Header';
+import Header from './components/Header/Header.jsx';
+import Main from './components/Main/Main.jsx';
+
 const App = () => {
-return (
-  <Header />
-)
+  return (
+    <>
+      <Header />
+      <Main></Main>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import './Main.css';
+
+const Main = () => {
+    return (
+        <main>
+            <h1>Welcome to My App</h1>
+            <p>This is the main content area.</p>
+        </main>
+    )
+}
+
+export default Main;
