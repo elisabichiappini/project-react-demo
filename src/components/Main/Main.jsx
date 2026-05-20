1e99cb4c2873d2bb1bdadbdf5dffa3d25830f2af
elisabichiappini <elisabichiappini@gmail.com>
2026-05-20 16:00:49 +0200

    feat: add card contacts

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,8 +6,10 @@ const Main = () => {
         <main>
             <h1>Welcome to My App</h1>
             <p>This is the main content area.</p>
-            <CardContacts />
-            <CardContacts />
+            <div className="cards">
+                <CardContacts />
+                <CardContacts />
+            </div>
         </main>
     )
 }
